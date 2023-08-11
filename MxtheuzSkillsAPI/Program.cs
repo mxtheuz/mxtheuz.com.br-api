cdaffea6ff3bb549f62df45cc9063ee91744a67d
Matheus <90228238+ofmxtheuuz@users.noreply.github.com>
2023-08-11 12:50:18 -0300

    Refactor(program.cs): remove comments from build.

--- MxtheuzSkillsAPI/Program.cs
+++ MxtheuzSkillsAPI/Program.cs
@@ -3,8 +3,6 @@ using MxtheuzSkillsAPI.Utils.Services;
 using MxtheuzSkillsAPI.Utils.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddScoped<IGistRequest, GistRequest>();
@@ -12,11 +10,9 @@ builder.Services.AddScoped<IPingService, PingService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
